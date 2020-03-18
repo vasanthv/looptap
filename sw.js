b27b0917c9d1592b9ee928b082c055f19acb85db
Vasanth.V <imvasanthv@gmail.com>
2020-03-18 09:21:11 +0530

    Added service worker & CNAME

---
+++ sw.js
@@ -0,0 +1,37 @@
+/**
+ * Looptap Service worker
+ */
+
+const version = '1.0.0';
+const currentCacheName = 'looptap-v' + version;
+
+self.addEventListener('install', function(e) {
+	console.log('Install event triggered. New updates available.');
+	const filesToCache = ['/', '/manifest.json'];
+
+	// Deleting the previous version of cache
+	e.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames
+					.filter(cacheName => cacheName != currentCacheName)
+					.map(cacheName => caches.delete(cacheName))
+			);
+		})
+	);
+
+	// add the files to cache
+	e.waitUntil(
+		caches.open(currentCacheName).then(function(cache) {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(cache) {
+			return cache || fetch(event.request);
+		})
+	);
+});
