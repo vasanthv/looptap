4ea7c858b535dea4160f348d3fa4d3252ed27b7e
Vasanth.V <imvasanthv@gmail.com>
2020-03-18 14:20:29 +0530

    Added spacebar press

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Looptap Service worker
  */
 
-const version = '1.0.4';
+const version = '1.0.5';
 const currentCacheName = 'looptap-v' + version;
 
 self.addEventListener('install', function(e) {
