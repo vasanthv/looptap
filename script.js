1b7ad92f5564cae010bfbbebbb2dca7afb5dd31b
vasanth <imvasanthv@gmail.com>
2023-04-29 21:48:00 -0400

    Updated vue js version

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const loopTapApp = new Vue({
-	el: "#canvas",
-	data: {
-		arc: [180, 270],
-		taps: 0,
-		score: 0,
-		best: window.localStorage.best || 0,
-		state: "init",
-		prevTapTime: 0,
-		colors: [
-			"#ED5565",
-			"#D9444F",
-			"#ED5F56",
-			"#DA4C43",
-			"#F87D52",
-			"#E7663F",
-			"#FAB153",
-			"#F59B43",
-			"#FDCE55",
-			"#F6BA43",
-			"#C2D568",
-			"#B1C353",
-			"#99D469",
-			"#83C251",
-			"#42CB70",
-			"#3CB85D",
-			"#47CEC0",
-			"#3BBEB0",
-			"#4FC2E7",
-			"#3CB2D9",
-			"#5C9DED",
-			"#4C8CDC",
-			"#9398EC",
-			"#7277D5",
-			"#CC93EF",
-			"#B377D9",
-			"#ED87BF",
-			"#D870AE",
-		],
+/* global Vue */
+const loopTapApp = Vue.createApp({
+	data() {
+		return {
+			arc: [180, 270],
+			taps: 0,
+			score: 0,
+			best: window.localStorage.best || 0,
+			state: "init",
+			prevTapTime: 0,
+			colors: [
+				"#ED5565",
+				"#D9444F",
+				"#ED5F56",
+				"#DA4C43",
+				"#F87D52",
+				"#E7663F",
+				"#FAB153",
+				"#F59B43",
+				"#FDCE55",
+				"#F6BA43",
+				"#C2D568",
+				"#B1C353",
+				"#99D469",
+				"#83C251",
+				"#42CB70",
+				"#3CB85D",
+				"#47CEC0",
+				"#3BBEB0",
+				"#4FC2E7",
+				"#3CB2D9",
+				"#5C9DED",
+				"#4C8CDC",
+				"#9398EC",
+				"#7277D5",
+				"#CC93EF",
+				"#B377D9",
+				"#ED87BF",
+				"#D870AE",
+			],
+		};
 	},
 	computed: {
-		arcDValue: function() {
+		arcDValue() {
 			return this.describeArc(50, 50, 40, this.arc[0], this.arc[1]);
 		},
 	},
 	methods: {
-		polarToCartesian: function(centerX, centerY, radius, angleInDegrees) {
+		polarToCartesian(centerX, centerY, radius, angleInDegrees) {
 			const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
 			return {
 				x: centerX + radius * Math.cos(angleInRadians),
@@ -52,7 +54,7 @@ const loopTapApp = new Vue({
 			};
 		},
 
-		describeArc: function(x, y, radius, startAngle, endAngle) {
+		describeArc(x, y, radius, startAngle, endAngle) {
 			const start = this.polarToCartesian(x, y, radius, endAngle);
 			const end = this.polarToCartesian(x, y, radius, startAngle);
 			const arcFlag = endAngle - startAngle <= 180 ? "0" : "1";
@@ -60,7 +62,7 @@ const loopTapApp = new Vue({
 			return d;
 		},
 
-		getAngle: function(cx, cy, ex, ey) {
+		getAngle(cx, cy, ex, ey) {
 			const dy = ey - cy;
 			const dx = ex - cx;
 			let theta = Math.atan2(dx, -dy);
@@ -69,7 +71,7 @@ const loopTapApp = new Vue({
 			return theta;
 		},
 
-		getBallAngle: function() {
+		getBallAngle() {
 			const bg = document.getElementById("bg").getBoundingClientRect();
 			const bgCenter = { x: bg.left + bg.width / 2, y: bg.top + bg.height / 2 };
 			const ball = document.getElementById("ball").getBoundingClientRect();
@@ -77,29 +79,29 @@ const loopTapApp = new Vue({
 			return this.getAngle(bgCenter.x, bgCenter.y, ballCenter.x, ballCenter.y);
 		},
 
-		setArc: function() {
+		setArc() {
 			const random = (i, j) => Math.floor(Math.random() * (j - i)) + i;
-			arc = [];
+			let arc = [];
 			arc.push(random(0, 300));
 			arc.push(random(arc[0] + 10, arc[0] + 110));
 			arc[1] = arc[1] > 360 ? 360 : arc[1];
 			this.arc = arc;
 		},
 
-		startPlay: function() {
+		startPlay() {
 			this.state = "started";
 			this.taps = 0;
 			this.score = 0;
 			this.prevTapTime = Date.now();
 		},
-		stopPlay: function() {
+		stopPlay() {
 			if (this.state === "started") {
 				this.state = "stopped";
 				if (this.score > this.best) window.localStorage.best = this.best = this.score;
 			}
 		},
 
-		tap: function(e) {
+		tap(e) {
 			e.preventDefault();
 			e.stopPropagation();
 			if (this.state === "started") {
@@ -116,7 +118,7 @@ const loopTapApp = new Vue({
 			}
 		},
 	},
-});
+}).mount("#canvas");
 
 if ("ontouchstart" in window) {
 	window.addEventListener("touchstart", loopTapApp.tap);
