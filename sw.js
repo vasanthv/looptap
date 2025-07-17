87972ec98ee23af1781fb70b3fae94e41a35dba1
vasanth <imvasanthv@gmail.com>
2025-07-17 17:26:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,31 +5,39 @@
 const version = "1.1.6";
 const currentCacheName = "looptap-v" + version;
 
-self.addEventListener("install", function(e) {
-	console.log("Install event triggered. New updates available.");
-	const filesToCache = ["/", "/favicon.ico", "/vue.min.js", "/style.css", "/script.js", "/manifest.json"];
+self.addEventListener("install", function (e) {
+  console.log("Install event triggered. New updates available.");
+  const filesToCache = [
+    "/",
+    "/favicon.ico",
+    "/style.css",
+    "/script.js",
+    "/manifest.json",
+  ];
 
-	// Deleting the previous version of cache
-	e.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.filter((cacheName) => cacheName != currentCacheName).map((cacheName) => caches.delete(cacheName))
-			);
-		})
-	);
+  // Deleting the previous version of cache
+  e.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName != currentCacheName)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
 
-	// add the files to cache
-	e.waitUntil(
-		caches.open(currentCacheName).then(function(cache) {
-			return cache.addAll(filesToCache);
-		})
-	);
+  // add the files to cache
+  e.waitUntil(
+    caches.open(currentCacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(cache) {
-			return cache || fetch(event.request);
-		})
-	);
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cache) {
+      return cache || fetch(event.request);
+    })
+  );
 });
